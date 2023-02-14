1c5849fa72ff0367c0ef0d4128f28dceb5396798
Luis Fernando Aguirre <luis.aguirre@plataforma10.com>
2023-02-14 12:38:18 -0300

    Se agrega generic result para mostrar valores con status en los endpoints que lo requieran

---
+++ GamesStation.SuperExampleGame.Domain/Results/GenericResult.cs
@@ -0,0 +1,27 @@
+﻿namespace GamesStation.SuperExampleGame.Domain.Results
+{
+    public class GenericResult<T>
+    {
+        public bool IsSucceeded { get; set; }
+        public string Message { get; set; }
+
+        public T Value { get; set; }
+
+        public GenericResult(bool isSucceeded, string message, T value)
+        {
+            this.IsSucceeded = isSucceeded;
+            this.Message = message;
+            this.Value = value;
+        }
+
+        public static GenericResult<T> SetOk(T value)
+        {
+            return new GenericResult<T>(true, null, value);
+        }
+
+        public static GenericResult<T> SetFail(string message)
+        {
+            return new GenericResult<T>(false, message, (T)default);
+        }
+    }
+}
